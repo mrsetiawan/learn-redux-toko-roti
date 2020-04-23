6e84501a72bb9151e3b496a784a49e94cf411fc1
rahmat <muhamadrahmatsetiawan@gmail.com>
2020-04-24 06:41:39 +0700

    add styling component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 import store from './store';
+import styled from 'styled-components';
 
-function App() {
-  console.log(store)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  render() {
+
+    // const Container = styled.div`
+    //   display: flex;
+    //   flex-flow: row wrap;
+    //   width: 900px;
+    //   height:auto;
+    //   margin: 50px auto;
+    //   box-sizing: border-box;
+    //   justify-content: center;
+    //   border:1px solid white;
+    //   padding: 15px;
+    // `
+    // 🍩 &nbsp; 🍰 &nbsp; 🛒 &nbsp; 🍩
+    return (
+      <div className='container'>
+        <div className='header'>
+          <h1>🍰</h1>
+          <h3>Toko Roti Vzlooks</h3>
+        </div>
+        <div className='section-top'>
+          <p> 🍩 0 Donat &nbsp;&nbsp;  🍰 0 Cake</p>
+        </div>
+        <div className='header'>
+          <button>Tepung 100</button>
+          <button>Cokelat 100</button>
+          <button>Cherry 50</button>
+        </div>
+        <div className='section-bottom'>
+          <h3>🍩</h3>
+          <h3>Buat 1 Donat</h3>
+        </div>
+        <div className='section-bottom'>
+          <h3>🍰</h3>
+          <h3>Buat 1 Cake</h3>
+        </div>
+        <div className='section-bottom'>
+          <h3>🛒</h3>
+          <h3>Isi Ulang Bahan</h3>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
